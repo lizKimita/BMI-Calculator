e6921f0a7a33b716afb8ac3c2962a94ed5c313a9
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-12 22:05:15 +0300

    adding the cm squared

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 var divide = function(weight, height) {
- return weight / height;
+ return weight / (height);
 };
 
 var weight = prompt ("Enter your weight in KGS here:");
 var height = prompt ("Enter your height in centimeters here:");
-var results = divide(weight, height);
+var results = divide(weight, (height*height));
 alert(results.toFixed(2));
